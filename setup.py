bdf9b372c8f1ea87a59a662e7f026ee8770ebad7
⭕Alexander Rymdeko-Harvey <8761706+killswitch-GUI@users.noreply.github.com>
2018-06-14 13:11:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='simplydomain',
-      version='1.0.17',
+      version='1.0.19',
       description='simplydomain is a very basic framework to automate domain brute forcing.',
       url='http://github.com/SimplySecurity/simplydomain-pkg',
       author='Alexander Rymdeko-Harvey',
